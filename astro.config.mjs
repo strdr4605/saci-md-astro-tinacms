a15ff16769c814da3c9f547b9f10181cf2086af3
Dragoș Străinu <str.dr4605@gmail.com>
2025-02-23 16:20:35 +0200

    Use astro-umami integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
+
+import yeskunallumami from "@yeskunall/astro-umami";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    yeskunallumami({ id: "66be9a3f-6a8b-4cc7-9104-bf59fbf772d2" }),
+  ],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  adapter: netlify()
+  adapter: netlify(),
 });
